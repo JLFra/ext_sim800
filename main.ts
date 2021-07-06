7b2c2fb9398e2a557ed6b602f507a336b42cdcf4
julien launay <julien.launay@laposte.net>
2021-07-06 16:53:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+namespace sim8000{
+
+    //% block="Connexion au système SIM8000 || RX $Rx TX $Tx"
+    //% expandableArgumentMode="toggle"
+    //% Rx.defl=SerialPin.P0 Tx.defl=SerialPin.P14
+    export function connect_lora(Rx: SerialPin, Tx: SerialPin): void {
+        basic.showIcon(IconNames.Asleep)
+        serial.setRxBufferSize(100)
+        serial.setTxBufferSize(100)
+        serial.redirect(
+        Tx,
+        Rx,
+        BaudRate.BaudRate9600
+        )
+        basic.pause(2000)
+        basic.showIcon(IconNames.SmallSquare)
+        let recept_data = ""
+        serial.writeString("AT")
+        recept_data = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
+        basic.showString(recept_data)
+        basic.pause(1000)
+        if (recept_data == "OK") { 
+            basic.showString("C")
+        }
+        else {
+            basic.showIcon(IconNames.Square)
+            basic.pause(500)
+        }
+        
+    }
+    //% block="Envoi $donnee"
+    //% donnee.defl='essai'
+    export function envoi_donnee(donnee: string): void {
+        serial.writeString(donnee+"#")
+    }
+
+    //% block="Donnee reçue"
+    export function donnee_recue(): string {
+        return serial.readUntil(serial.delimiters(Delimiters.Hash));
+    }
+}
